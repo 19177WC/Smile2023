b0cf3ca9616a3b8118e9fa877eaa56cc9f0ad2af
19177 <19177@wc.school.nz>
2023-02-21 11:08:10 +1300

    sgaega

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
+DATABASE = "smilecafe.db"
 app = Flask(__name__)
+
+def open_database(db_name):
+    try:
+        connection = sqlite3.connect(db_name)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_home():  # put application's code here
+    return render_template("home.html")
+
+
+@app.route('/menu/<cat_id>')
+def render_menu(cat_id):  # put application's code here
+    con = open_database(DATABASE)
+    query = "SELECT * FROM category"
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
+    print(category_list)
+    return render_template("menu.html", catergories=category_list)
+
+
+@app.route('/contact')
+def render_contact():  # put application's code here
+    return render_template("contact.html")
 
 
 if __name__ == '__main__':
